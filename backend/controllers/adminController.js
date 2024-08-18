c73dc175a92b4add5c9b945f887d40b80e9acd2b
sarvagya005 <sarvagyasingh4516@gmail.com>
2024-08-18 20:43:26 +0530

    changes

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -331,7 +331,7 @@ const deleteClass = asyncHandler(async (req, res) => {
       } else {
         await Student.updateMany(
           { classroom: id },
-          { $set: { classroom: undefined, classroomName:undefined } }
+          { $set: { classroom: undefined, className:undefined } }
         );
         const teacher = await Teacher.findById(classroom.teacher);
         if (teacher) {
